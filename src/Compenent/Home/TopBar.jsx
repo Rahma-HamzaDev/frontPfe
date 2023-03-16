e32fc015cc34d675365a7e21b9297bb02fa3cb81
Rahma Hamza <rahmaa.hamza.2001@gmail.com>
2023-03-16 21:30:55 +0100

    topbar

--- src/Compenent/Home/TopBar.jsx
+++ src/Compenent/Home/TopBar.jsx
@@ -16,10 +16,10 @@ function TopBar() {
         <div className="col-md-6 text-center text-lg-end">
           <div className="position-relative d-inline-flex align-items-center bg-primary text-white top-shape px-5">
             <div className="me-3 pe-3 border-end py-2">
-              <p className="m-0"><i className="fa fa-envelope-open me-2"></i>info@example.com</p>
+              <p className="m-0"><i className="fa fa-envelope-open me-2"></i>Admin@example.com</p>
             </div>
             <div className="py-2">
-              <p className="m-0"><i className="fa fa-phone-alt me-2"></i>+012 345 6789</p>
+              <p className="m-0"><i className="fa fa-phone-alt me-2"></i>+216 25 369 147</p>
             </div>
           </div>
         </div>
